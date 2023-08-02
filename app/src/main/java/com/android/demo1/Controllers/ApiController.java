404ebe4f613b49e706d444ad9136cfb4e59c52e1
LeCheWang <haphonggoroki@gmail.com>
2023-08-02 23:09:16 +0700

    demo upload file

--- app/src/main/java/com/android/demo1/Controllers/ApiController.java
+++ app/src/main/java/com/android/demo1/Controllers/ApiController.java
@@ -6,13 +6,16 @@ import com.google.gson.GsonBuilder;
 
 import java.util.List;
 
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface ApiController {
     String DOMAIN = "https://api.shoe.gorokiapp.com/api/";
@@ -29,5 +32,11 @@ public interface ApiController {
 
     @POST("shoes")
     Call<Shoe> createShoe(@Body() RequestBody shoe);
+
+    @POST("shoes")
+    @Multipart
+    Call<Shoe> createShoe(@Part("name") RequestBody name, @Part("description") RequestBody description, @Part("price") RequestBody price, @Part MultipartBody.Part img);
+
+
 
 }
--- app/src/main/java/com/android/demo1/Fragments/Home/HomeFragment.java
+++ app/src/main/java/com/android/demo1/Fragments/Home/HomeFragment.java
@@ -1,12 +1,26 @@
 package com.android.demo1.Fragments.Home;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +29,11 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.demo1.Controllers.ApiController;
+import com.android.demo1.Helper.RealPathUtil;
 import com.android.demo1.Models.Shoe;
 import com.android.demo1.R;
 import com.android.demo1.databinding.FragmentHomeBinding;
@@ -29,10 +45,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
@@ -41,12 +59,19 @@ import retrofit2.Response;
 
 
 public class HomeFragment extends Fragment {
+    private static final int MY_REQUEST_CODE = 10;
+
     public HomeFragment() {
         // Required empty public constructor
     }
     FragmentHomeBinding binding;
     List<Shoe> shoes = new ArrayList<>();
     ShoeAdapter adapter;
+
+    ImageView imgAvt;
+
+    Uri mUri;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -80,13 +105,22 @@ public class HomeFragment extends Fragment {
                 dialog.setCancelable(true);
 
                 EditText edtName, edtDes, edtPrice;
+
                 Button btnCreate;
 
                 edtName = dialog.findViewById(R.id.edtName);
                 edtDes = dialog.findViewById(R.id.edtDes);
                 edtPrice = dialog.findViewById(R.id.edtPrice);
+                imgAvt = dialog.findViewById(R.id.imgAvt);
 
                 btnCreate = dialog.findViewById(R.id.btnCreate);
+
+                imgAvt.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onClickRequestPermission();
+                    }
+                });
 
                 btnCreate.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -97,14 +131,19 @@ public class HomeFragment extends Fragment {
                         //check name > 6 kys tuwj
                         //call api
 //                        Shoe shoe = new Shoe(name, des, Long.parseLong(price));
-                        RequestBody requestBody = new MultipartBody.Builder()
-                                .setType(MultipartBody.FORM)
-                                .addFormDataPart("name", name)
-                                .addFormDataPart("description", des)
-                                .addFormDataPart("price", price)
-                                .build();
+                        RequestBody bodyName = RequestBody.create(MediaType.parse("multipart/form-data"), name);
+                        RequestBody bodyDes = RequestBody.create(MediaType.parse("multipart/form-data"), des);
+                        RequestBody bodyPrice = RequestBody.create(MediaType.parse("multipart/form-data"), price);
 
-                        ApiController.apiService.createShoe(requestBody).enqueue(new Callback<Shoe>() {
+                        String realPath = RealPathUtil.getRealPath(getActivity(), mUri);
+                        File file = new File(realPath);
+
+                        RequestBody requestBodyAvt = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+
+                        MultipartBody.Part multipartBodyAvt = MultipartBody.Part.createFormData("img", file.getName(), requestBodyAvt);
+
+
+                        ApiController.apiService.createShoe(bodyName, bodyDes, bodyPrice, multipartBodyAvt).enqueue(new Callback<Shoe>() {
                             @Override
                             public void onResponse(Call<Shoe> call, Response<Shoe> response) {
                                 if (response.isSuccessful()){
@@ -171,4 +210,60 @@ public class HomeFragment extends Fragment {
             }
         });
     }
+
+    public void onClickRequestPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            openGallary();
+            return;
+        }
+        if (getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            openGallary();
+//            startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+        }else {
+            String [] requestPermission= {Manifest.permission.READ_EXTERNAL_STORAGE};
+            getActivity().requestPermissions(requestPermission, MY_REQUEST_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==MY_REQUEST_CODE){
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openGallary();
+            }
+        }
+
+    }
+
+    private void openGallary() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mActivityResultLauncher.launch(Intent.createChooser(intent, "Select Picture"));
+    }
+
+    private ActivityResultLauncher<Intent> mActivityResultLauncher= registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode()== RESULT_OK){
+                        Intent data= result.getData();
+                        if (data==null){
+                            return;
+                        }
+                        Uri uri = data.getData();
+                        mUri = uri;
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), uri);
+                            imgAvt.setImageBitmap(bitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
 }
